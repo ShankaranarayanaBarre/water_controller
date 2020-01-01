b7da5b79385414dfd75d15b7976c717abd262786
ShankaranarayanaBarre <shankar.narayana901@gmail.com>
2020-01-01 12:28:18 +0530

    Added the project files.

---
+++ water_controller/main.c
@@ -0,0 +1,21 @@
+/*
+ * water_controller.c
+ *
+ * Created: 12/30/2019 2:56:08 PM
+ * Author : shnx
+ */ 
+
+
+#include "sam.h"
+
+
+int main(void)
+{
+    /* Initialize the SAM system */
+    SystemInit();
+
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
